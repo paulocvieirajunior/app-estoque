329aded6d2ba193552e8cf1e069566f2c2b240e0
Paulo Vieira <paulocvieirajunior@gmail.com>
2026-06-17 15:27:05 -0300

    Adiciona a tela principal do aplicativo

--- app/src/main/java/com/example/estoque/SecondFragment.java
+++ app/src/main/java/com/example/estoque/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,19 +21,47 @@ public class SecondFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        String perfil = getArguments() != null ? getArguments().getString("perfil") : "";
+        configurarMenuPorPerfil(perfil);
         binding.buttonSecond.setOnClickListener(v ->
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment)
         );
+    }
+
+    private void configurarMenuPorPerfil(String perfil) {
+        binding.btnCadastro.setVisibility(View.GONE);
+        binding.btnEntrada.setVisibility(View.GONE);
+        binding.btnVenda.setVisibility(View.GONE);
+        binding.btnSeparacao.setVisibility(View.GONE);
+
+        switch (perfil) {
+            case "gerente":
+                binding.btnCadastro.setVisibility(View.VISIBLE);
+                binding.btnEntrada.setVisibility(View.VISIBLE);
+                binding.btnVenda.setVisibility(View.VISIBLE);
+                binding.btnSeparacao.setVisibility(View.VISIBLE);
+                break;
+            case "estoquista":
+                binding.btnEntrada.setVisibility(View.VISIBLE);
+                break;
+            case "vendedor":
+                binding.btnVenda.setVisibility(View.VISIBLE);
+                break;
+            case "embalador":
+                binding.btnSeparacao.setVisibility(View.VISIBLE);
+                break;
+            default:
+                Toast.makeText(getContext(), "Perfil desconhecido", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     @Override
@@ -40,5 +69,4 @@ public class SecondFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
